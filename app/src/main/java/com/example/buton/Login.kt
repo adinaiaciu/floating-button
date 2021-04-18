b3688a6d2a4b79111bf0f0bf8897aecb9b0d66e3
DESKTOP-HVDBARJ\ml <ma27lazar@gmail.com>
2021-04-18 21:19:05 +0300

    poza LogIn

--- app/src/main/java/com/example/buton/Login.kt
+++ app/src/main/java/com/example/buton/Login.kt
@@ -3,7 +3,9 @@ package com.example.buton
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
@@ -15,52 +17,119 @@ import androidx.core.app.ActivityCompat
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_login.*
-
+import java.io.ByteArrayOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class Login : AppCompatActivity() {
-    private lateinit var auth: FirebaseAuth
+    private var mAuth:FirebaseAuth?=null
+
+    private var database=FirebaseDatabase.getInstance()
+    private var myRef=database.reference
+
+
+//    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
-        auth = Firebase.auth
+//        auth = Firebase.auth
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        buttonSignUp.setOnClickListener {
-            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-            inputMethodManager.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
-            if (editTextEmail.text.toString().isNullOrEmpty() || editTextPassword.text.toString()
-                    .isNullOrEmpty())
-                textViewResponse.text = "Email Address or Password is not provided"
-            else {
-                auth.createUserWithEmailAndPassword(
-                    editTextEmail.text.toString(),
-                    editTextPassword.text.toString())
-                    .addOnCompleteListener(this) { task ->
-                        if (task.isSuccessful) {
-                            textViewResponse.text =
-                                "Sign Up successfull. Email and Password created"
-                            val user = auth.currentUser
-                            //updateUI(user)
-                        } else {
-                            textViewResponse.text = "Sign Up Failed"
-                            //updateUI(null)
-                        }
-                    }
-            }
-            fun updateUI(currentUser: FirebaseUser?) {
 
-            }
-        }
+    mAuth= FirebaseAuth.getInstance()
 
 
+//        buttonSignUp.setOnClickListener {
+//            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+//            inputMethodManager.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+//            if (editTextEmail.text.toString().isNullOrEmpty() || editTextPassword.text.toString()
+//                    .isNullOrEmpty())
+//                textViewResponse.text = "Email Address or Password is not provided"
+//            else {
+//                auth.createUserWithEmailAndPassword(
+//                    editTextEmail.text.toString(),
+//                    editTextPassword.text.toString())
+//                    .addOnCompleteListener(this) { task ->
+//                        if (task.isSuccessful) {
+//                            textViewResponse.text =
+//                                "Sign Up successfull. Email and Password created"
+//                            val user = auth.currentUser
+//                            updateUI(user)
+//                        } else {
+//                            textViewResponse.text = "Sign Up Failed"
+//                            updateUI(null)
+//                        }
+//                    }
+//            }
+//            fun updateUI(currentUser: FirebaseUser?) {
+//
+//            }
+//        }
+//
+//
         imagePerson.setOnClickListener(View.OnClickListener {
             checkPermission()
         })
     }
 
+    fun LoginToFireBase (email:String, password:String){
+
+        mAuth!!.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this){ task->
+                if(task.isSuccessful) {
+                    Toast.makeText(applicationContext, "Successful login", Toast.LENGTH_LONG).show()
+
+                    var currentUser = mAuth!!.currentUser
+                    SaveImageInFireBase(currentUser)
+                }else
+                {
+                    Toast.makeText(applicationContext,"Fail to login",Toast.LENGTH_LONG).show()
+                }
+
+            }
+
+    }
+
+  fun SaveImageInFireBase(currentUser:FirebaseUser){
+      val email:String=currentUser!!.email.toString()
+      val storage=FirebaseStorage.getInstance()
+      val storageRef=storage.getReferenceFromUrl("gs://reciclarity1.appspot.com")
+      val df= SimpleDateFormat("ddMMyyHHmmss")
+      val dataobj= Date()
+      val imagePAth=SplitString(email) +  "." + df.format(dataobj)+".jpg"
+      val ImageRef=storageRef .child("images/"+imagePAth)
+
+      imagePerson.isDrawingCacheEnabled=true
+      imagePerson.buildDrawingCache()
+
+      val drawable=imagePerson.drawable as BitmapDrawable
+      val bitmap=drawable .bitmap
+      val baos=ByteArrayOutputStream()
+      bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+      val data =baos.toByteArray()
+      val uploadTask=ImageRef.putBytes(data)
+      uploadTask.addOnFailureListener{
+          Toast.makeText(applicationContext,"Fail to upload",Toast.LENGTH_LONG).show()
+      } .addOnSuccessListener{taskSnapshot ->
+          var DownloadURL=taskSnapshot.getMetadata()!!.getReference()!!.getDownloadUrl()
+          myRef.child("Users").child(currentUser.uid).child("email").setValue(currentUser.email)
+          myRef.child("Users").child(currentUser.uid).child("ProfileImage").setValue(DownloadURL)
+          Toast.makeText(applicationContext,"Successful upload",Toast.LENGTH_LONG).show()
+      }
+  }
+
+fun SplitString(email:String):String{
+    val split=email.split("@")
+    return split[0]
+}
+
 
     val READIMAGE:Int=234
+
     fun checkPermission(){
 
         if(Build.VERSION.SDK_INT>=23){
@@ -91,6 +160,7 @@ class Login : AppCompatActivity() {
     }
 
     val PICK_IMAGE_CODE=123
+
     fun loadImage(){
         var intent=Intent(Intent.ACTION_PICK,
             android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
@@ -100,14 +170,13 @@ class Login : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        if (requestCode==PICK_IMAGE_CODE && data!=null && resultCode==RESULT_OK){
+        if (requestCode==PICK_IMAGE_CODE && data!=null){
             val selectedImage=data.data
             val filePathColum=arrayOf(MediaStore.Images.Media.DATA)
             val cursor= contentResolver.query(selectedImage!!, filePathColum,null,null,null)
             //cursor!!.moveToFirst()
             //am adaugat !! la selectedImage
             if (cursor != null) {
-                //cursor.moveToFirst()
                 cursor.moveToFirst()
             }
             //initial cursor.moveToFirst()
@@ -116,15 +185,15 @@ class Login : AppCompatActivity() {
             val coulomIndex=cursor!!.getColumnIndex(filePathColum[0])
             val picturePAth= cursor.getString(coulomIndex)
             //val picturePAth=cursor.getString(coulomIndex)
-            if (cursor != null) {
-                cursor.close()
-            }
-            //cursor.close()
+//            if (cursor != null) {
+//                cursor.close()
+//            }
+            cursor.close()
             imagePerson.setImageBitmap(BitmapFactory.decodeFile(picturePAth))
         }
     }
 
     fun butLogIn(view:View) {
-
+        LoginToFireBase (editTextEmail.text.toString(), editTextPassword.text.toString())
     }
 }
--- app/src/main/java/com/example/buton/feed.kt
+++ app/src/main/java/com/example/buton/feed.kt
@@ -1,32 +1,39 @@
 package com.example.buton
 
 import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_feed.*
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.add_ticket.view.*
 
 class feed : AppCompatActivity() {
-    var ListTweets=ArrayList<Ticket>()
-    var adpater: MyTweetAdpater?=null
+    var ListTweets = ArrayList<Ticket>()
+    var adpater: MyTweetAdpater? = null
+    var myemail:String?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_feed)
 
-
+        var b: Bundle? =intent.extras
+        myemail= b?.getString("email")
         ListTweets.add(Ticket("0", "him", "url", "add"))
         ListTweets.add(Ticket("0", "him", "url", "clara"))
         ListTweets.add(Ticket("0", "him", "url", "clara"))
         ListTweets.add(Ticket("0", "him", "url", "clara"))
 
-        adpater=MyTweetAdpater(this, ListTweets)
-        lvTweets.adapter=adpater
+        adpater = MyTweetAdpater(this, ListTweets)
+        lvTweets.adapter = adpater
     }
 
-    inner class MyTweetAdpater: BaseAdapter {
+    inner class MyTweetAdpater : BaseAdapter {
         var listNotesAdapter = ArrayList<Ticket>()
         var context: Context? = null
 
@@ -41,8 +48,10 @@ class feed : AppCompatActivity() {
 
             if (mytweet.postPersonUID.equals("add")) {
                 var myView = layoutInflater.inflate(R.layout.add_ticket, null)
-                //load add ticket
-                //TODO: work
+
+                myView.file.setOnClickListener(View.OnClickListener {
+
+                })
                 return myView
             } else {
                 var myView = layoutInflater.inflate(R.layout.posts, null)
@@ -68,4 +77,39 @@ class feed : AppCompatActivity() {
 
         }
     }
+
+    val PICK_IMAGE_CODE = 123
+    fun loadImage() {
+
+        var intent = Intent(
+            Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        )
+        startActivityForResult(intent, PICK_IMAGE_CODE)
+    }
+
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        if (requestCode == PICK_IMAGE_CODE && data != null && resultCode == RESULT_OK) {
+//
+//            val selectedImage = data.data
+//            val filePathColum = arrayOf(MediaStore.Images.Media.DATA)
+//            val cursor = contentResolver.query(selectedImage!!, filePathColum, null, null, null)
+//            cursor!!.moveToFirst()
+//            val coulomIndex = cursor!!.getColumnIndex(filePathColum[0])
+//            val picturePath = cursor!!.getString(coulomIndex)
+//            cursor!!.close()
+//            UploadImage(BitmapFactory.decodeFile(picturePath))
+//        }
+//
+//    }
+
+//    fun UploadImage (bitmap:Bitmap){
+//        val storage=FirebaseStorage.getInstance()
+//        val storgaRef=storage.getReferenceFromUrl("gs://twitterwebservice-b75b6.appspot.com")
+//        val df= SimpleDateFormat("ddMMyyHHmmss")
+//        val dataobj= Date()
+//        val imagePath= SaveSettings.userID + "."+ df.format(dataobj)+ ".jpg"
+//        val ImageRef=storgaRef.child("imagePost/"+imagePath
+//    }
 }
